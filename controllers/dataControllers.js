60176bf44e82276e2e0bc0829a9c80a06b8524bb
Alexandre Caffarelli <caffarelli.alexandre@gmail.com>
2020-08-04 19:23:17 +0200

    Added joi validator for mug post request using hapi-joi

---
+++ controllers/dataControllers.js
@@ -0,0 +1,27 @@
+const Joi = require ('@hapi/joi');
+
+module.exports = {
+
+    validateMug: async (request, response, next) => {
+        try {
+            const JoiSchema = Joi.object({
+                label: Joi.string().pattern(/^(([A-Za-z0-9]+)(\s|$))+/).required(),
+                capacity: Joi.number().integer().required(),
+                last_usage: Joi.date().timestamp(),
+                is_clean: Joi.boolean(),
+                description: Joi.string(),
+                state: Joi.string()
+            });
+            const validation = JoiSchema.validate(request.body);
+            if (validation.error) {
+                response.status(400).json(validation.error.details);
+            } else {
+                console.log('Validated Data');
+                next();
+            };
+        } catch (error) {
+            response.statut(500).json(error);
+        }
+    },
+
+} 
