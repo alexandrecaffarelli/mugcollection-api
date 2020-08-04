91b6c1f4c625e091b7ef211e8b04f9092e23567b
Alexandre Caffarelli <caffarelli.alexandre@gmail.com>
2020-08-04 19:25:36 +0200

    Small tweak to model methods to take into account 'collection' schema

--- models/Mug.js
+++ models/Mug.js
@@ -4,13 +4,13 @@ module.exports = class Mug {
 
     // static method to pull all mugs from database
     static async findAll() {
-        const result = await client.query('SELECT * FROM "get_mugs"();');
+        const result = await client.query('SELECT * FROM "collection"."get_mugs"();');
         return result.rows;
     };
 
     // static method to pull a single mug from database using its id
     static async findById(id) {
-        const result = await client.query('SELECT * FROM "get_mug"($1);', [id]);
+        const result = await client.query('SELECT * FROM "collection"."get_mug"($1);', [id]);
         const mug = new Mug();
         Object.assign(mug, result.rows[0]);
         return mug;
@@ -18,19 +18,19 @@ module.exports = class Mug {
 
     // prototype method to modify data (is_clean, last_usage) from instantiated mug into database
     async use() {
-        const result = await client.query('SELECT * FROM "use_mug"($1);', [this.id]);
+        const result = await client.query('SELECT * FROM "collection"."use_mug"($1);', [this.id]);
         Object.assign(this, result.rows[0]);
     };
 
     // prototype method to modify data (is_clean) from instantiated mug into database    
     async clean() {
-        const result = await client.query('SELECT * FROM "clean_mug"($1);', [this.id]);
+        const result = await client.query('SELECT * FROM "collection"."clean_mug"($1);', [this.id]);
         Object.assign(this, result.rows[0]);
     };
 
     // prototype method to insert instantiated mug into database
     async save() {
-        const result = await client.query('SELECT * FROM "new_mug"($1);', [this]);
+        const result = await client.query('SELECT * FROM "collection"."new_mug"($1);', [this]);
         this.id = result.rows[0].id;
     };
 
