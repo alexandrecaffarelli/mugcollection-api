aa6c78f3069b9bef784cff474ee39c52af8f5255
Alexandre Caffarelli <caffarelli.alexandre@gmail.com>
2020-08-07 19:40:26 +0200

    Added 404 error handling for '/' route

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const mugRouter = require('./router/mugRouter');
 const app = express();
 
 app.use(express.json());
+
 app.use('/mugs', mugRouter);
+
+app.use((_, response) => {
+    response.status(404).json({
+        data: "Sorry can't find that!"
+    });
+});
 
 const port = process.env.PORT || 3000;
 
